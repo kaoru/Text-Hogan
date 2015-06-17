2a31f82f6efac742616d709a596496b1647109b6
Alex Balhatchet <kaoru@slackwise.net>
2015-06-17 22:06:48 +0100

    Handy debugging script hogan-bin.pl

---
+++ hogan-bin.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature 'say';
+
+use lib '/home/alex/Documents/Git/Hogan.pm/lib';
+
+use Hogan::Compiler;
+use Hogan::Template;
+
+my $c = Hogan::Compiler->new();
+
+my $text = "Hello, {{name}}";
+
+my $s = $c->scan($text);
+
+my $p = $c->parse($s, $text);
+
+my $g = $c->generate($p, $text, {});
+
+my $o = $g->render({ name => "Alex" });
+
+say $o;
