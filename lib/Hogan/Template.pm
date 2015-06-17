4f5122eeb3807ea0df769f613cca1680907543f3
Alex Balhatchet <kaoru@slackwise.net>
2015-06-17 08:10:00 +0100

    Finish up Template... but not the hairiest partial handling method thing ;-)

--- lib/Hogan/Template.pm
+++ lib/Hogan/Template.pm
@@ -99,4 +99,213 @@ sub ep {
     return $template;
 }
 
+# tries to find a partial in the current scope and render it
+sub rp {
+    my ($self, $symbol, $context, $partials, $indent) = @_;
+    my $partial = $self->ep($symbol, $partials);
+    if (!$partial) {
+        return "";
+    }
+
+    return $partial->ri($context, $partials, $indent);
+}
+
+# render a section
+sub rs {
+    my ($self, $context, $partials, $section) = @_;
+    my $tail = $context->[-1];
+    if (ref $tail ne 'ARRAY') {
+        section($context, $partials, $self);
+        return;
+    }
+
+    for (my $i = 0; $i < @$tail; $i++) {
+        push @$context, $tail->[$i];
+        section($context, $partials, $self);
+        pop @$context;
+    }
+}
+
+# maybe start a section
+sub s {
+    my ($self, $val, $ctx, $partials, $inverted, $start, $end, $tags) = @_;
+    my $pass;
+    if (ref($val) eq 'ARRAY' && !@$val) {
+        return 0;
+    }
+
+    if (ref($val) eq 'CODE') {
+        $val = $self->ms($val, $ctx, $partials, $inverted, $start, $end, $tags);
+    }
+
+    $pass = !!$val;
+
+    if (!$inverted && $pass && $ctx) {
+        push @$ctx, (ref($val) eq 'HASH') ? $val : $ctx->[-1];
+    }
+
+    return $pass;
+}
+
+# find values with dotted names
+sub d {
+    my ($self, $key, $ctx, $partials, $return_found) = @_;
+    my $found;
+    my @names = split m/[.]/, $key;
+    my $val = $self->f($names[0], $ctx, $partials, $return_found);
+    my $do_model_get = $self->{'options'}{'model_get'};
+    my $cx;
+
+    if ($key eq '.' && (ref($ctx->[-2]) eq 'ARRAY')) {
+        $val = $ctx->[-1];
+    }
+    else {
+        for (my $i = 1; $i < @names; $i++) {
+            $found = find_in_scope($names[$i], $val, $do_model_get);
+            if (defined $found) {
+                $cx = $val;
+                $val = $found;
+            }
+            else {
+                $val = "";
+            }
+        }
+    }
+}
+
+# find values with normal names
+sub f {
+    my ($self, $key, $ctx, $partials, $return_found) = @_;
+    my $val = 0;
+    my $v;
+    my $found = 0;
+    my $do_model_get = $self->{'otions'}{'model_get'};
+
+    for (my $i = @$ctx - 1; $i >= 0; $i--) {
+        $v = $ctx->[$i];
+        $val = find_in_scope($key, $v, $do_model_get);
+        if (defined $val) {
+            $found = 1;
+            last;
+        }
+    }
+
+    if (!$found) {
+        return $return_found ? 0 : "";
+    }
+
+    if (!$return_found && (ref($val) eq 'CODE')) {
+        $val = $self->mv($val, $ctx, $partials);
+    }
+
+    return $val;
+}
+
+# higher order templates
+sub ls {
+    my ($self, $func, $cx, $ctx, $partials, $text, $tags) = @_;
+    my $old_tags = $self->{'options'}{'delimiters'};
+
+    $self->{'options'}{'delimiters'} = $tags;
+    $self->b($self->ct(coerce_to_string($func->($self,$cx,$text,$ctx)), $cx, $partials));
+    $self->{'options'}{'delimiters'} = $old_tags;
+
+    return 0;
+}
+
+# compile text
+sub ct {
+    my ($self, $text, $cx, $partials) = @_;
+    if ($self->{'options'}{'disable_lambda'}) {
+        die "Lambda features disabled";
+    }
+    return $self->{'c'}->compile($text, $self->{'options'})->render($cx, $partials);
+}
+
+# template result buffering
+sub b {
+    my ($self, $s) = @_;
+    $self->{'buf'} .= $s;
+}
+
+sub fl {
+    my ($self) = @_;
+    my $r = $self->{'buf'};
+    $self->{'buf'} = "";
+    return $r;
+}
+
+# method replace section
+sub ms {
+    my ($self, $func, $ctx, $partials, $inverted, $start, $end, $tags) = @_;
+    my $text_source;
+    my $cx = $ctx->[-1];
+    my $result = $func->($self, $cx);
+
+    if (ref($result) eq 'CODE') {
+        if ($inverted) {
+            return 1;
+        }
+        else {
+            $text_source = ($self->{'active_sub'} && $self->{'subs_text'} && $self->{'subs_text'}{$self->{'active_sub'}})
+                ? $self->{'subs_text'}{$self->{'active_sub'}}
+                : $self->{'text'};
+            return $self->ls($result, $cx, $ctx, $partials, substr($text_source,$start,$end), $tags);
+        }
+    }
+
+    return $result;
+}
+
+# method replace variable
+sub mv {
+    my ($self, $func, $ctx, $partials) = @_;
+    my $cx = $ctx->[-1];
+    my $result = $func->($self,$cx);
+
+    if (ref($result) eq 'CODE') {
+        return $self->ct(coerce_to_string($result->($self,$cx)), $cx, $partials);
+    }
+
+    return $result;
+}
+
+sub sub {
+    my ($self, $name, $context, $partials, $indent) = @_;
+    my $f = $self->{'subs'}{$name};
+    if ($f) {
+        $self->{'active_sub'} = $name;
+        $f->($context,$partials,$self,$indent);
+        $self->{'active_sub'} = "";
+    }
+}
+
+################################################
+
+sub find_in_scope {
+    my ($key, $scope, $do_model_get) = @_;
+    my $val;
+
+    if (defined $scope->{$key}) {
+        $val = $scope->{$key};
+    }
+    elsif ($do_model_get) {
+        die "Do Model Get not implemented in Hogan.pm!";
+    }
+
+    return $val;
+}
+
+sub create_specialized_partial {
+    my ($instance, $subs, $partials, $stack_subs, $stack_partials, $stack_text) = @_;
+
+    die "Hope we don't get here because this looks hairy!!";
+}
+
+
+sub coerce_to_string {
+    my ($str) = @_;
+    return $str // "";
+}
+
 1;
